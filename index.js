1408f2d7c60b5ed7f2428bb1b1fe11e75b919d61
Nausheen-S <60954697+Nausheen-S@users.noreply.github.com>
2020-04-02 20:09:51 +0800

    added second function

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var converthex = function(hex){
   } : null;
 }
 console.log(converthex(process.argv[2]));
+
+var convertrgb = function (r, g, b) {
+   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+ }
+ console.log(convertrgb(255, 255, 255));
