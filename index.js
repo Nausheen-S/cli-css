ba752313aa1565d2f23e503c1dfcdec0fdbf8ed6
Nausheen-S <60954697+Nausheen-S@users.noreply.github.com>
2020-04-02 20:52:33 +0800

    tried hsl conversion

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const my = require('./calculate.js');
 var answer1 = process.argv[2];
 var answer2 = process.argv[3];
-var r = parseInt(process.argv[2])
-var g = parseInt(process.argv[3])
-var b = parseInt(process.argv[4])
+var r = parseInt(process.argv[3]);
+var g = parseInt(process.argv[4]);
+var b = parseInt(process.argv[5]);
+var h = parseInt(process.argv[3]);
+var s = parseInt(process.argv[4]);
+var l = parseInt(process.argv[5]);
+
 if (answer1 === "hex"){
      console.log(my.converthex(answer2));
  }
- else {
+ else if (answer1 === "rgb") {
      console.log(my.convertrgb(r,g,b));
+ }
+ else{
+     console.log(my.rgb2hsl(h,s,l));
  }
